56b0489550e098b4a4b5a70d47701f6fb7d1abd9
josh <jcrtll@protonmail.com>
2021-03-28 12:39:29 -0700

    Add files via upload
    Added win, lose and draw functions. Used convertToWord to clean up messaging. Learned about ` ${} `.

--- rockpaperscissors.js
+++ rockpaperscissors.js
@@ -1,65 +1,79 @@
-let playerScore = 0;
+let userScore = 0;
 let computerScore = 0;
+const userScore_span = document.getElementById("user-score");
+const computerScore_span = document.getElementById("computer-score");
+const scoreBoard_div = document.querySelector(".score-board");
+const result_p = document.querySelector(".result > p");
+const rock_div = document.getElementById("r");
+const paper_div = document.getElementById("p");
+const scissors_div = document.getElementById("s");
 
-function computerChoice() {
-  let choices = ['rock', 'paper', 'scissors'];
-  let result = choices[Math.floor(Math.random() * choices.length)];
-  return result
-  };
+function getComputerChoice() {
+  const choices = ['r', 'p', 's'];
+  const randomNumber = [Math.floor(Math.random() * choices.length)];
+  return choices[randomNumber];
+}
 
-// rework playerChoice so it runs based on a click event on whatever button is chosen
-// seems like i will need to create code that looks at button class (currently namebtn) and turns that into choice for player
+function convertToWord(letter) {
+  if (letter === "r") return "Rock";
+  if (letter === "p") return "Paper";
+  return "Scissors";
+}
 
-function playerChoice() {
-  let playerChoice = prompt('Rock, paper or scissors?');
-    return playerChoice.toLowerCase();
+function win(userChoice, computerChoice) {
+  userScore++;
+  userScore_span.innerHTML = userScore;
+  computerScore_span.innerHTML = computerScore;
+  result_p.innerHTML = `${convertToWord(userChoice)} beats ${convertToWord(computerChoice)} You win!`;
+}
+
+function lose(userChoice, computerChoice) {
+  computerScore++;
+  userScore_span.innerHTML = userScore;
+  computerScore_span.innerHTML = computerScore;
+  result_p.innerHTML = convertToWord(computerChoice) + " beats " + convertToWord(userChoice) + ". You lost! ";
+}
+
+function draw(userChoice, computerChoice) {
+  userScore_span.innerHTML = userScore;
+  computerScore_span.innerHTML = computerScore;
+  result_p.innerHTML = convertToWord(computerChoice) + " is the same as " + convertToWord(userChoice) + ". It's a tie! ";
 }
 
 
-
-function playRound(choice1, choice2) {
-  if (choice1 == 'rock' && choice2 == 'paper') {
-    userWin = true;
-    return ('Paper beats rock! You lose!');
-  } else if (choice1 == 'rock' && choice2 == 'scissors') {
-    userWin = true;
-    return ('Rock smash scissors! You win!');
-  } else if (choice1 == 'paper' && choice2 == 'rock') {
-    userWin = true;
-    return ('Paper covers rock! You win!');
-  } else if (choice1 == 'paper' && choice2 == 'scissors') {
-    computerWin = true;
-    return ('Scissors cuts paper! You lose.'); 
-    } else if (choice1 == 'scissors' && choice2 == 'paper') {
-    userWin = true;
-    return ('Scissors cuts paper! You win!'); 
-}   else if (choice1 == 'scissors' && choice2 == 'rock') {
-    computerWin = true;
-    return ('Rock beats scissors! You lose!');
- } else if(choice1 == choice2) {
-    return ('Tie! You must select a different weapon!');
+function game(userChoice) {
+  const computerChoice = getComputerChoice();
+  switch (userChoice + computerChoice) {
+    case "rs":
+    case "pr":
+    case "sp":
+        win(userChoice, computerChoice);
+        break;
+    case "rp":
+    case "ps":
+    case "sr":
+        lose(userChoice, computerChoice);
+        break;
+    case "rr":
+    case "pp":
+    case "ss":
+        draw(userChoice, computerChoice);
+        break;
  }
 }
 
-// function game() {
-//         for (var i = 0; i < 5; i++) {
-//             var playerSelection = prompt('Rock, paper or scissors?');
-//             var computerSelection = computerChoice();
-//             console.log("You chose: " + playerSelection);
-//             console.log("The computer chose: " + computerSelection);
-//             playRound(playerSelection, computerSelection);
-            
-//             console.log(playRound(playerSelection, computerSelection));
-        
-//             if (userWin = true) {
-//             playerScore++;
-//             console.log("You won the round! Your score is ", playerScore, "and the computer's score is ", computerScore);
-//         }   else if (computerWin = true) {
-//             computerScore++;
-//             console.log("You lost the round! Your score is ", playerScore, "and the computer's score is ", computerScore);   
-//  }   else {
-//       console.log("It's a tie!");
-//  }
-//       }
-//     }
+function main() {
+  rock_div.addEventListener('click', function() {
+    game("r");
+  })
 
+  paper_div.addEventListener('click', function() {
+    game("p");
+  })
+  scissors_div.addEventListener('click', function() {
+    game("s");
+  })
+
+}
+
+main();
